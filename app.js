0c1ccef16df4d115b87a2a0d093cc3f39819a2bd
Alejo Oviedo <alejoemmanueloviedo@gmail.com>
2025-10-10 19:35:03 -0300

    Implementacion primera entrega

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get("/api/products/", async (req,res)=>{
         res.status(200).json({state: true, productos, mesagge: "Lista de productos"})
     }
     catch(error){
-        res.status(500).json({state: false, mesagge: error.mesagge})
+        res.status(500).json({state: false, message: error.message})
     }
 })
 app.get("/api/products/:pid", async (req,res)=>{
@@ -30,7 +30,7 @@ app.get("/api/products/:pid", async (req,res)=>{
         res.status(200).json({state: true, producto, mesagge: `Producto con PID: ${pid}`})
     }
     catch(error){
-        res.status(500).json({state: false, mesagge: error.mesagge})
+        res.status(500).json({state: false, messagge: error.message})
     }
 })
 
@@ -39,10 +39,10 @@ app.post("/api/products/",async (req,res)=>{
     try{
         const producto = new Product(req.body);
         await productManager.addProduct(producto);
-        res.status(201).json({state: true, producto, mesagge: `Producto Agregado: ${producto.title}`})
+        res.status(201).json({state: true, producto, message: `Producto Agregado: ${producto.title}`})
     }
     catch(error){
-        res.status(500).json({state: false, mesagge: error.mesagge})
+        res.status(500).json({state: false, message: error.message})
     }
 })
 
@@ -52,10 +52,10 @@ app.put("/api/products/:pid", async (req,res)=>{
         const pid = req.params.pid;
         const updates = req.body;
         const producto = (await productManager.updateProduct(pid,updates)).productoActualizado;
-        res.status(200).json({state: true, producto, mesagge: `Producto Actualizado: ${producto.title}`})
+        res.status(200).json({state: true, producto, message: `Producto Actualizado: ${producto.title}`})
     }
     catch(error){
-        res.status(500).json({state: false, mesagge: error.mesagge})
+        res.status(500).json({state: false, message: error.message})
     }
 })
 
@@ -64,10 +64,10 @@ app.delete("/api/products/:pid", async (req,res)=>{
     try{
         const pid = req.params.pid;
         const producto = (await productManager.deleteProduct(pid)).deleteP;
-        res.status(200).json({state: true, producto, mesagge: `Producto Eliminado`})
+        res.status(200).json({state: true, producto, message: `Producto Eliminado`})
     }
     catch(error){
-        res.status(500).json({state: false, mesagge: error.mesagge})
+        res.status(500).json({state: false, message: error.message})
     }
 })
 
@@ -77,17 +77,50 @@ app.delete("/api/products/:pid", async (req,res)=>{
 app.get("/api/carts", async (req,res)=>{
     try{
         const carritos = await cartManager.getC();
-        res.status(200).json({state: true, carritos, mesagge: "Lista de carritos"})
+        res.status(200).json({state: true, carritos, message: "Lista de carritos"})
     }
     catch(error){
-        res.status(500).json({state: false, mesagge: error.mesagge})
+        res.status(500).json({state: false, message: error.message})
+    }
+});
+
+
+app.get("/api/carts/:cid", async (req,res)=>{
+    try{
+        const cid = req.params.cid;
+        const carrito = (await cartManager.getCID(cid)).encontrado;
+        res.status(200).json({state: true, carrito: carrito.products, message: "Lista de carritos"})
+    }
+    catch(error){
+        res.status(500).json({state: false, message: error.message})
+    }
+});
+
+app.post("/api/carts/", async (req,res)=>{
+    try{
+        const products = req.body.products;
+        if(!products) throw new Error("No hay productos en el carrito")
+        const carro = products.map(p => p = new PAndQuantity(p))
+        const carritos = (await cartManager.addC(new Cart(carro))).carritos
+        res.status(201).json({state:true, carritos, message: "Carrito agregado"})
+    }
+    catch(error){
+        res.status(500).json({state: false, message: error.message})
     }
 })
 
 
-
-
-
+app.post("/api/carts/:cid/product/:pid", async (req,res)=>{
+    try{
+        const cid = req.params.cid;
+        const pid = req.params.pid;
+        const carritos = (await cartManager.addQuantity(cid,pid)).carritos
+        res.status(200).json({state: true,carritos, message: `Se agrego el producto ${pid} al carrito ${cid}`})
+    }
+    catch(error){
+        res.status(500).json({state: false, message: error.message})
+    }
+})
 
 app.listen(8080, ()=>{
     console.log("Servidor iniciado en el puerto 8080")
